4058708a6bb6174fb1633fc460c85277d1cc0434
Ibrahim Hanna <ihanna@hotmail.com>
2019-06-11 13:18:27 -0400

    deply a cluster of web servers

--- main.tf
+++ main.tf
@@ -10,20 +10,21 @@ variable "server_port" {
 	description = "The port the server will use for HTTP requests"
 	default = 8080
 }
+data "aws_availability_zones" "all" {}
 
 #IAC Section
 ############
-resource "aws_instance" "example" {
-	ami = "ami-40d28157"
+resource "aws_launch_configuration" "example" {
+	image_id = "ami-40d28157"
 	instance_type = "t2.micro"
-	vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+	security_groups = ["${aws_security_group.instance.id}"]
 	user_data = <<-EOF
 		    #!/bin/bash
 		    echo "Hello, World" > index.html 
 		    nohup busybox httpd -f -p "${var.server_port}" &
 		    EOF
-	tags  = {
-		Name = "terraform-example"
+	lifecycle {
+		create_before_destroy = true
 	}
 }
 resource "aws_security_group" "instance" {
@@ -34,11 +35,27 @@ resource "aws_security_group" "instance" {
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
+	
+	lifecycle {
+                create_before_destroy = true
+        }
+}
+
+resource "aws_autoscaling_group" "example" {
+	launch_configuration = "${aws_launch_configuration.example.id}"
+	availability_zones = "${data.aws_availability_zones.all.names}"
+	min_size = 2
+	max_size = 10
+	tag {
+		key = "Name"
+		value = "terraform-asg-example"
+		propagate_at_launch = true
+	}
 }
 
 # Output Section
 ################
-output "public_ip" {
-	value = "${aws_instance.example.public_ip}"
-}
+#output "public_ip" {
+#	value = "${aws_instance.example.public_ip}"
+#}
 

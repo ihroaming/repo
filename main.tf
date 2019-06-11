dd7ebe7e87e1859a1d48e1c8715f6820bca2bbb3
Ibrahim Hanna <ihanna@hotmail.com>
2019-06-11 13:46:28 -0400

    add load balancer

--- main.tf
+++ main.tf
@@ -30,8 +30,8 @@ resource "aws_launch_configuration" "example" {
 resource "aws_security_group" "instance" {
 	name = "terraform-example-instance"
 	ingress {
-		from_port = 8080
-		to_port = 8080
+		from_port = "${var.server_port}"
+		to_port = "${var.server_port}"
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
@@ -52,10 +52,43 @@ resource "aws_autoscaling_group" "example" {
 		propagate_at_launch = true
 	}
 }
+resource "aws_elb" "example" {
+	name = "terraform-asg-example"
+	availability_zones = "${data.aws_availability_zones.all.names}"
+	security_groups = ["${aws_security_group.elb.id}"]
+	listener {
+		lb_port = 80
+		lb_protocol = "http"
+		instance_port = "${var.server_port}"
+		instance_protocol = "http"
+		}
+	health_check {
+		healthy_threshold = 2 
+		unhealthy_threshold = 2 
+		timeout = 3 
+		interval = 30 
+		target = "HTTP:${var.server_port}/"
+	}
+}
+resource "aws_security_group" "elb" {
+	name = "terraform-example-elb"
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+		}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
 
 # Output Section
 ################
-#output "public_ip" {
-#	value = "${aws_instance.example.public_ip}"
-#}
+output "elb_dns_name" {
+	value = "${aws_elb.example.dns_name}"
+}
 
